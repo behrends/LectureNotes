058d84172bc8a6cf505dfa1f21db167af828debe
Erik Behrends <behrends@users.noreply.github.com>
2020-09-28 15:16:37 +0200

    Button in Activity mit Listener ausstatten

--- app/src/main/java/com/example/lecturenotes/MainActivity.kt
+++ app/src/main/java/com/example/lecturenotes/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.lecturenotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // finde den Button mit der ID button2 im UI
+        val button = findViewById<Button>(R.id.button2)
+        // setze einen Listener/Callback an den Button (Klickverhalten für Button)
+        button.setOnClickListener {
+            Toast.makeText(this, "Mit Listener!", Toast.LENGTH_LONG).show()
+        }
     }
 }

1aa7ddbccafc73f2b6043aa57b4cc4a6606bebe0
Erik Behrends <behrends@users.noreply.github.com>
2020-09-28 16:03:34 +0200

    setContentView mit binding.root

--- app/src/main/java/com/example/lecturenotes/MainActivity.kt
+++ app/src/main/java/com/example/lecturenotes/MainActivity.kt
@@ -9,6 +9,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root) // setContentView(R.layout.activity_main)
         binding.button2.setOnClickListener {
             Toast.makeText(this, "Mit Listener!", Toast.LENGTH_LONG).show()
             binding.textView4.text = "TITEL GEÄNDERT"

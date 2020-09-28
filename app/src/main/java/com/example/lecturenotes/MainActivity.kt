34db89140b48448119e0135c29541e6bab8f52a2
Erik Behrends <behrends@users.noreply.github.com>
2020-09-28 15:52:17 +0200

    View Binding aktiviert und verwendet

--- app/src/main/java/com/example/lecturenotes/MainActivity.kt
+++ app/src/main/java/com/example/lecturenotes/MainActivity.kt
@@ -2,19 +2,16 @@ package com.example.lecturenotes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
+import com.example.lecturenotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val title = findViewById<TextView>(R.id.textView4)
-        val button = findViewById<Button>(R.id.button2)
-        button.setOnClickListener {
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding.button2.setOnClickListener {
             Toast.makeText(this, "Mit Listener!", Toast.LENGTH_LONG).show()
-            title.text = "TITEL GEÄNDERT"
+            binding.textView4.text = "TITEL GEÄNDERT"
         }
     }
 }

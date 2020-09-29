934d1dc17b88654763775dd8ca1eb8201014157d
Erik Behrends <behrends@users.noreply.github.com>
2020-09-29 12:12:27 +0200

    Bearbeiten und abbrechen implementiert

--- app/src/main/java/com/example/lecturenotes/MainActivity.kt
+++ app/src/main/java/com/example/lecturenotes/MainActivity.kt
@@ -3,13 +3,14 @@ package com.example.lecturenotes
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.example.lecturenotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root) // setContentView(R.layout.activity_main)
         binding.button2.setOnClickListener {
             val intent = Intent(this, EditActivity::class.java)
@@ -20,11 +21,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
-        val title = data?.getStringExtra("NOTE_TITLE")
         if(resultCode == 100) {
-            Toast.makeText(this, "SPEICHERN!", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "ABBRECHEN!", Toast.LENGTH_SHORT).show()
+            val title = data?.getStringExtra("NOTE_TITLE")
+            binding.textView4.text = title
         }
     }
 }

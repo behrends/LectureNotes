f6b6e66c9c696ea65e35d4b2078753680a3ea5fd
Erik Behrends <behrends@users.noreply.github.com>
2020-09-30 11:46:28 +0200

    Von DetailFragment zu EditFragment

--- app/src/main/java/com/example/lecturenotes/DetailFragment.kt
+++ app/src/main/java/com/example/lecturenotes/DetailFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class DetailFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +14,10 @@ class DetailFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        val view = inflater.inflate(R.layout.fragment_detail, container, false)
+        view.findViewById<FloatingActionButton>(R.id.button2).setOnClickListener {
+            view.findNavController().navigate(R.id.action_detailFragment_to_editFragment)
+        }
+        return view
     }
 }

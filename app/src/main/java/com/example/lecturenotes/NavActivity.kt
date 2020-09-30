c64cbd683ea52a9c2ca4ea42760ba5c6ce9655f8
Erik Behrends <behrends@users.noreply.github.com>
2020-09-30 09:41:21 +0200

    Neue NavActivity

---
+++ app/src/main/java/com/example/lecturenotes/NavActivity.kt
@@ -0,0 +1,11 @@
+package com.example.lecturenotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NavActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nav)
+    }
+}

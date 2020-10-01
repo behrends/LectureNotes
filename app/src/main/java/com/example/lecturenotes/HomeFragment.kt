d8b97b3a5c59782dae4e515280f582e961c660e3
Erik Behrends <behrends@users.noreply.github.com>
2020-10-01 10:30:12 +0200

    RecyclerView im fragment_home.xml

--- app/src/main/java/com/example/lecturenotes/HomeFragment.kt
+++ app/src/main/java/com/example/lecturenotes/HomeFragment.kt
@@ -4,10 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -16,14 +13,6 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
-        view.findViewById<Button>(R.id.button1).setOnClickListener {
-            val bundle = bundleOf("noteTitle" to "Notiz1") // TODO tatsächlicher Titel
-            view.findNavController().navigate(R.id.action_homeFragment_to_detailFragment, bundle)
-        }
-        view.findViewById<Button>(R.id.button2).setOnClickListener {
-            val bundle = bundleOf("noteTitle" to "Notiz2") // TODO tatsächlicher Titel
-            view.findNavController().navigate(R.id.action_homeFragment_to_detailFragment, bundle)
-        }
         return view
     }
 }

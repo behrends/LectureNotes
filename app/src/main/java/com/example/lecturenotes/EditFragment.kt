527bd44f8296021e13816dd60b19680911155b32
Erik Behrends <behrends@users.noreply.github.com>
2020-10-01 09:41:11 +0200

    Nach Bearbeiten: Up-Button führt von DetailFragment zum HomeFragment

--- app/src/main/java/com/example/lecturenotes/EditFragment.kt
+++ app/src/main/java/com/example/lecturenotes/EditFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.lecturenotes.databinding.FragmentEditBinding
@@ -16,7 +17,8 @@ class EditFragment : Fragment() {
     ): View? {
         val binding = FragmentEditBinding.inflate(layoutInflater)
         binding.saveButton.setOnClickListener { view: View ->
-            view.findNavController().navigate(R.id.action_editFragment_to_detailFragment)
+            val bundle = bundleOf("noteTitle" to binding.editNoteTitle.text.toString())
+            view.findNavController().navigate(R.id.action_editFragment_to_detailFragment, bundle)
         }
         return binding.root
     }

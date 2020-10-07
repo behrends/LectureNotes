d18b36a628452e2ed5a055aa68ef1e3c6c74fa74
Erik Behrends <behrends@users.noreply.github.com>
2020-10-07 11:03:07 +0200

    Firebase testweise  eingebaut

--- app/src/main/java/com/example/lecturenotes/HomeFragment.kt
+++ app/src/main/java/com/example/lecturenotes/HomeFragment.kt
@@ -1,11 +1,14 @@
 package com.example.lecturenotes
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.lecturenotes.databinding.FragmentHomeBinding
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.async
 
@@ -21,8 +24,27 @@ class HomeFragment : Fragment() {
 
         // DB-Verbindung herstellen und Dao initialisieren
         GlobalScope.async {
-            val noteDao = AppDatabase.getDatabase(requireContext()).noteDao()
-            val notes = noteDao.getNotes()
+//            val noteDao = AppDatabase.getDatabase(requireContext()).noteDao()
+//            val notes = noteDao.getNotes()
+            var notes = mutableListOf<Note>()
+
+            // TODO: Firebase importieren, init fehlt noch
+            val db = Firebase.firestore
+            var index = 0L
+            db.collection("notes").get()
+                .addOnSuccessListener { result ->
+                    for (note in result) {
+                        Log.d("HOME_FRAGMENT", "${note.id} => ${note.data}")
+                        // TODO Note data class soll ID-String haben --> verwende ID aus Cloud
+                        val newNote = Note(note.data.get("title").toString(), note.data.get("text").toString(), index)
+                        notes.add(newNote)
+                        index++
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w("HOME_FRAGMENT", "Error getting documents.", exception)
+                }
+
             adapter.setData(notes)
         }
 
